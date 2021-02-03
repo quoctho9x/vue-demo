e06733d42603e74c6e0faff78b0658ceb7a45067
thonq <thonq@vng.com.vn>
2021-02-03 13:59:30 +0700

    add vuex in project

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home test</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/intro">Intro</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :count='count' />
+    <div v-if="count > 2 ">
+      Now you see me
+    </div>
+    <div v-else>
+      Now you don't
+    </div>
+
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+    <button @click='increment'> click vao di {{count}} {{count > 2 ? 'them vao ': ''}}</button>
   </div>
 </template>
 
@@ -13,6 +23,19 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return {count: this.$store.state.count, awesome: false}
+  },
+  methods:{
+    increment() {
+
+      this.$store.commit('increment');
+      this.count = this.$store.state.count;
+      this.awesome = true;
+      console.log(this.$store.state.count)
+
+    }
   }
 }
 </script>
